c31385160dc3589c8580ce19bc15f95dc37f20d0
wiston <gonzalesjair54@gmail.com>
2020-05-21 22:27:26 -0500

    css terminado

---
+++ funtion/operaciones.js
@@ -0,0 +1,23 @@
+var cero = document.getElementById("0");
+var uno = document.getElementById("1");
+var dos = document.getElementById("2");
+var tres = document.getElementById("3");
+var cuatrp = document.getElementById("4");
+var cinco = document.getElementById("5");
+var seis = document.getElementById("6");
+var siete = document.getElementById("7");
+var ocho = document.getElementById("8");
+var nueve = document.getElementById("9");
+var punto = document.getElementById("punto");
+var resultado = document.getElementById("resultado") 
+
+//operaciones
+var sumas = document.getElementById("sumar");
+var restas = document.getElementById("restar");
+var multiplicaciones = document.getElementById("multiplicar");
+var divisiones = document.getElementById("dividir");
+
+ 
+
+//clicks
+
